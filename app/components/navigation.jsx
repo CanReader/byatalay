4f343c241e0da5331cb9401e59f1b2b2c4653e8e
Canberk <canberk_nurcan@hotmail.com>
2024-08-15 14:37:58 +0300

    The navigation has been updated

--- app/components/navigation.jsx
+++ app/components/navigation.jsx
@@ -1,59 +1,21 @@
 import { Open_Sans } from "next/font/google";
+import './../../public/styles/navigation.css';
 
 const sans = Open_Sans({ subsets: ["latin"] });
 
+const pages = ["Sayfa", "Hakkımızda", "Menu", "İletişim"];
+
 export default function() {
     return(
-    <nav style={style.nav}>
-        <img style={style.img} src="images/Logo.svg"/>
-        <ul style={style.list}>
-            <li style={style.item}><a className={sans.className}>Sayfa</a></li>
-            <li style={style.item}><a className={sans.className}>Hakkımızda</a></li>
-            <li style={style.item}><a className={sans.className}>Menu</a></li>
-            <li style={style.item}><a className={sans.className}>İletişim</a></li>
+    <nav className="nav">
+        <img className='img' src="images/Logo.svg"/>
+        <ul className='list'>
+        {pages.map((text, index) => (
+                    <li className="item" key={index}>
+                        <a className={sans.className}>{text}</a>
+                    </li>
+                ))}
         </ul>
-        <a style={style.rez} className={sans.className}>Rezarvasyon</a>
+        <a className={'rez' + sans.className}>Rezarvasyon</a>
     </nav>);
 }
-
-const style = {
-    nav : {
-        width: '100%',
-        display: 'flex',
-        alignItems:'center',
-        backgroundColor:'var(--bgDark)',
-        padding:'5px 35px',
-        maxHeight:'65px',
-        position:'fixed',
-        top: 0,
-        left: 0,
-        zIndex:9999
-    },
-    img:{
-        display:'flex',
-        justifyContent:'flex-start',
-        alignItems:'center',
-        position:'relative',
-        height:35,
-        cursor:'pointer'
-    },
-    list:{
-        display:'inline',
-        listStyleType:'none',
-        flex: '1 1',
-        justifyContent: 'center',
-        alignItems: 'center',
-        textAlign: 'center',
-        listStyle: 'none',
-        cursor: 'pointer'
-    },
-    item:{
-        display:'inline',
-        margin:'0 1rem',
-        textDecoration:'none',
-    },
-    rez:{
-        display:'flex',
-        justifyContent: 'flex-end'
-    }
-};
